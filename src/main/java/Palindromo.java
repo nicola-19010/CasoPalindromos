a6ff2c32d91f6d1d5819ec077ba6accb47388846
nicola-19010 <n.pacheco02@ufromail.cl>
2022-09-11 23:28:21 -0300

    commit mejoras al metodo

--- src/main/java/Palindromo.java
+++ src/main/java/Palindromo.java
@@ -22,9 +22,17 @@ public class Palindromo {
 
     public static String pedirEntrada() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Por favor escriba una palabra:");
-        return scanner.nextLine();
+        String ingreso = "";
+
+        System.out.println("Ingrese un candidato a palindromo: ");
+        while (ingreso.isEmpty()){
+            System.out.println("Recuerde ingrese una opcion valida, no se permite texto vacio: ");
+            ingreso = scanner.nextLine();
+        }
+        return ingreso;
     }
+
+
 
 
 }
