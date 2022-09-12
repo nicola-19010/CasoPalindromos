7c0b77aefb4a10fbcb4e95bee0aed6681b820422
nicola-19010 <n.pacheco02@ufromail.cl>
2022-09-11 22:54:43 -0300

    commit Paso 3

--- src/test/java/PalindromoTest.java
+++ src/test/java/PalindromoTest.java
@@ -8,12 +8,18 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class PalindromoTest {
     private String[] palabrasIngresadas;
     private String[] palabrasIngresadas2;
+    private String[] palabrasIngresadasPasoTres;
+    private boolean[] resultadosEsperadosPasoTres;
     private boolean[] resultadosEsperados;
 
     @BeforeEach
     public void init(){
         palabrasIngresadas = new String[] {"200", "","Reconocer", "arbol", "la tele letal", "amA amA"};
         resultadosEsperados = new boolean[] {false, true, true, false, true, true};
+
+        palabrasIngresadasPasoTres = new String[] {"aca", "acas", "h"};
+        resultadosEsperadosPasoTres = new boolean[] {true, false, true};
+
     }
 
     @Test
@@ -22,6 +28,15 @@ public class PalindromoTest {
         for (int i = 0; i < palabrasIngresadas.length; i++) {
             boolean resultadoObtenido = Palindromo.esPalindromo(palabrasIngresadas[i]);
             assertEquals(resultadoObtenido, resultadosEsperados[i]);
+        }
+    }
+
+    @Test
+    @DisplayName("Caso de pruebas para el paso 3")
+    public void esPalindromoPasoTresTest(){
+        for (int i = 0; i < palabrasIngresadasPasoTres.length; i++) {
+            boolean resultadoObtenido = Palindromo.esPalindromo(palabrasIngresadasPasoTres[i]);
+            assertEquals(resultadoObtenido, resultadosEsperadosPasoTres[i]);
         }
     }
 
